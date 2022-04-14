ebbbd23d8782f4012f3b9ce703c90987245e0b38
Anderson VInicius Ribeiro <andyreborn@hotmail.com.br>
2022-04-14 11:01:57 -0300

    Adiciona método para pegar os dados do banco e instancia a classe de relatório detalhado

--- src/Main.java
+++ src/Main.java
@@ -9,31 +9,49 @@ import java.util.List;
 import bridge.exibicao.ExibeRelatorioNoConsole;
 import bridge.modelo.Produto;
 import bridge.relatorios.GeradorDeRelatorio;
+import bridge.relatorios.RelatorioDetalhado;
 import bridge.relatorios.RelatorioSimples;
 
 public class Main {
 
     public static void main(String[] args) {
-//        Connection connection = DatabaseConnection.getConnection();
-//        Connection connection2 = DatabaseConnection.getConnection();
-//        Connection connection3 = DatabaseConnection.getConnection();
+        Connection connection = DatabaseConnection.getConnection();
+
+//        List<Produto> produtos = new ArrayList<>();
 //
-//        System.out.println(connection);
-//        System.out.println(connection2);
-//        System.out.println(connection3);
+//        produtos.add(new Produto("Notebook", 2999.99));
+//        produtos.add(new Produto("Smartphone", 1999.99));
+//        produtos.add(new Produto("Airpods", 699.99));
+//        produtos.add(new Produto("Caneta inteligente", 99.99));
 
-        List<Produto> produtos = new ArrayList<>();
-
-        produtos.add(new Produto("Notebook", 2999.99));
-        produtos.add(new Produto("Smartphone", 1999.99));
-        produtos.add(new Produto("Airpods", 699.99));
-        produtos.add(new Produto("Caneta inteligente", 99.99));
-
-        System.out.println(produtos);
+        List<Produto> produtos = getProdutos(connection);
 
         ExibeRelatorioNoConsole erc = new ExibeRelatorioNoConsole();
         GeradorDeRelatorio rs = new RelatorioSimples(erc);
-        rs.geraRelatorios(produtos);
+        GeradorDeRelatorio rd = new RelatorioDetalhado(erc);
+//        rs.geraRelatorios(produtos);
+        rd.geraRelatorios(produtos);
+    }
 
+    public static List<Produto> getProdutos(Connection connection){
+        List<Produto> produtos = new ArrayList<>();
+
+        try{
+            Statement stmt = connection.createStatement();
+
+            ResultSet resultSet = stmt.executeQuery("select * from produtos " +
+                    "where status = true;");
+
+            while(resultSet.next()){
+                String nome = resultSet.getString("nome");
+                double preco = resultSet.getDouble("preco");
+
+                produtos.add(new Produto(nome, preco));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return produtos;
     }
 }
