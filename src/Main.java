3cad4cc1b56792ae04c077d43dd441e67ad2b4cb
Alex Corrêa <aelxbc@outlook.com>
2022-04-13 22:05:58 -0300

    Criação e execução da Classe Produto

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,33 @@ import DBConnection.DatabaseConnection;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import bridge.modelo.Produto;
 
 public class Main {
 
     public static void main(String[] args) {
-        Connection connection = DatabaseConnection.getConnection();
-        Connection connection2 = DatabaseConnection.getConnection();
-        Connection connection3 = DatabaseConnection.getConnection();
+//        Connection connection = DatabaseConnection.getConnection();
+//        Connection connection2 = DatabaseConnection.getConnection();
+//        Connection connection3 = DatabaseConnection.getConnection();
+//
+//        System.out.println(connection);
+//        System.out.println(connection2);
+//        System.out.println(connection3);
 
-        System.out.println(connection);
-        System.out.println(connection2);
-        System.out.println(connection3);
+        List<Produto> produtos = new ArrayList<>();
+
+        produtos.add(new Produto("Notebook", 2999.99));
+        produtos.add(new Produto("Smartphone", 1999.99));
+        produtos.add(new Produto("Airpods", 699.99));
+        produtos.add(new Produto("Caneta inteligente", 99.99));
+
+        System.out.println(produtos);
+
+
+
 
     }
 }
